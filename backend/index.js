304de8dcc5db885dfe9f4d110b54b52023ad8350
Yunus Emre Uyanık <y.emreuyanik@gmail.com>
2023-11-10 16:15:56 +0300

    cors policy added

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
-import booksRoute from './routes/booksRoute.js'
+import booksRoute from "./routes/booksRoute.js";
 
 // 1
 const app = express();
@@ -10,12 +10,26 @@ const app = express();
 // Middleware for parsing json request body
 app.use(express.json());
 
+// Middleware for handling cors policy
+
+// Option 1: Allow All Origins With default of cors(*)
+// app.use(cors());
+
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "https://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 // 2
 app.listen(PORT, () => {
   console.log(`listening on port:  + ${PORT}`);
 });
 
-app.use('/books', booksRoute);
+app.use("/books", booksRoute);
 
 mongoose
   .connect(mongoDBURL)
